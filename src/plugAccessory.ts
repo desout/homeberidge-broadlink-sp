39e1f6c07f4a84fe2a35dfd362e87be651b837bb
Andrei Anelkin <andrei.anelkin@ticketmaster.co.uk>
2023-04-02 21:22:49 +0300

    fixing issue with discovery

--- src/plugAccessory.ts
+++ src/plugAccessory.ts
@@ -32,7 +32,7 @@ export class PlugAccessory implements AccessoryPlugin {
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.displayName);
 
     // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.On)
